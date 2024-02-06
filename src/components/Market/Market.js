4f281bfaa180e84cde295cb562466e03526e883b
Oleksand-Zvirych <oaleks262@gmail.com>
2024-02-06 15:47:07 +0200

    edit

---
+++ src/components/Market/Market.js
@@ -0,0 +1,22 @@
+import React from "react";
+import HeaderWhite from '../Header/HeaderWhite'
+import './Market.css'
+
+
+const Market = () =>{
+
+    return(
+        <div className="market">
+            <div className='content-padding'>
+                <HeaderWhite/>
+                <div className="market-content">
+
+                </div>
+                </div>
+        </div>
+    )
+
+
+}
+
+export default Market;
