fdd5adf372dd767eb42fbbc4dc5433827eff9846
Oleksand-Zvirych <oaleks262@gmail.com>
2024-02-08 14:05:42 +0200

    edit

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,22 +1,126 @@
-import React from "react";
-import HeaderWhite from '../Header/HeaderWhite'
-import './Market.css'
+import React, { useState, useEffect } from "react";
+import HeaderWhite from '../Header/HeaderWhite';
+import './Market.css';
+import { lendingData } from '../../assets/api/api';
+import { setCartToLocalStorage } from '../Cart/localSave';
+
+const Market = () => {
+    const [products, setProducts] = useState([]); // Стан для зберігання списку товарів
+    const [cart, setCart] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(""); // Стан для зберігання обраної категорії
+    const [selectedSortOption, setSelectedSortOption] = useState("popularity"); // Значення "popularity" встановлено за замовчуванням
 
 
-const Market = () =>{
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await lendingData.getProduct();
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Помилка при отриманні товарів:", error);
+            }
+        };
+        fetchProducts();
+        handleSortChange("popularity");
+    }, []);
+    
+    const filterProductsByCategory = async (category) => {
+        setSelectedCategory(category); // Оновлення обраної категорії
+        try {
+            const response = await lendingData.getProduct(); // Отримати всі товари
+            const allProducts = response.data; // Отримати дані
+            let filteredProducts;
+            if (category === "Усі товари") {
+                // Показати всі товари
+                filteredProducts = allProducts;
+            } else {
+                // Показати товари, що належать до обраної категорії
+                filteredProducts = allProducts.filter(product => product.category === category);
+            }
+            // Сортувати товари після фільтрації
+            handleSortChange("popularity");
+            setProducts(filteredProducts); // Встановити відфільтровані товари
+        } catch (error) {
+            console.error("Помилка при отриманні товарів:", error);
+        }
+    };
+    
+    const handleSortChange = (value) => {
+        setSelectedSortOption(value);
+        let sortedProducts = [];
+    
+        switch (value) {
+            case "popularity":
+                sortedProducts = [...products].sort(() => Math.random() - 0.5);
+                break;
+            case "priceAsc":
+                sortedProducts = [...products].sort((a, b) => a.priceProduct - b.priceProduct);
+                break;
+            case "priceDesc":
+                sortedProducts = [...products].sort((a, b) => b.priceProduct - a.priceProduct);
+                break;
 
-    return(
+        }
+    
+        setProducts(sortedProducts);
+    };
+    
+    
+
+
+
+const addToCart = (product) => {
+    setCart([...cart, product]);
+    setCartToLocalStorage(product);
+  };
+    return (
         <div className="market">
             <div className='content-padding'>
                 <HeaderWhite/>
                 <div className="market-content">
+                    <div className="side-bar">                        
+                    <div className="sidebar">
+                            <h2>Фільтрація</h2>
+                   
+                            <select value={selectedCategory} onChange={(e) => filterProductsByCategory(e.target.value)}>
+                                <option value="Усі товари">Усі товари</option>
+                                <option value="Шипуча суміш для ванни">Шипуча суміш для ванни</option>
+                                <option value="Морська сіль для ванни">Морська сіль для ванни</option>
+                                <option value="Праліне">Праліне</option>
+                                <option value="Бомбочки для ванни">Бомбочки для ванни</option>
+                                <option value="Подарункові бокси">Подарункові бокси</option>
+                                <option value="Розпродаж">Розпродаж</option>
+                            </select>
 
+                            <h2>Сортування</h2>
+                            <select value={selectedSortOption} onChange={(e) => handleSortChange(e.target.value)}>
+                                <option value="popularity">Популярність</option>
+                                <option value="priceAsc">За зростанням ціни</option>
+                                <option value="priceDesc">За спаданням ціни</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="market-product">
+                        {/* Вікно відтворення товарів */}
+                        <div className="product-display">
+                            {/* Відображення списку товарів */}
+                            <ul className='minimarket-shop-ul'>
+                                {products.map(product => (
+                                    <li key={product._id} className='minimarket-shop-li'>
+                                        <img className='minimarket-shop-li-img' src={product.avatarUrl} alt={product.titleProduct} />
+                                        <h3 title={product.titleProduct} className='minimarket-shop-li-h3'>{product.titleProduct}</h3>
+                                        <p className='minimarket-shop-li-category'>{product.category}</p>
+                                        <p className='minimarket-shop-li-price'>{product.priceProduct} грн</p>
+                                        <a className='minimarket-shop-li-button' onClick={() => { addToCart(product); }} >Придбати</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                </div>
+            </div>
         </div>
-    )
-
-
-}
+    );
+};
 
 export default Market;
