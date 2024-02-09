16a81796c2993e44f7814c2dbbf37186a9e07b76
Oleksand-Zvirych <oaleks262@gmail.com>
2024-02-09 15:32:26 +0200

    edit

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -5,110 +5,105 @@ import { Link } from 'react-router-dom';
 import { lendingData } from '../../assets/api/api';
 import { setCartToLocalStorage } from '../Cart/localSave';
 
-
 const Market = () => {
-    const [products, setProducts] = useState([]); // Стан для зберігання списку товарів
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState(""); // Стан для зберігання обраної категорії
-    const [selectedSortOption, setSelectedSortOption] = useState("popularity"); // Значення "popularity" встановлено за замовчуванням
-    const [added, setAdded] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedSortOption, setSelectedSortOption] = useState("popularity");
 
+    const handleSortChange = (value) => {
+        setSelectedSortOption(value);
+        setProducts(SORT_OPTIONS[value]());
+    };
 
     useEffect(() => {
         const fetchProducts = async () => {
-          try {
-            const response = await lendingData.getProduct();
-            const allProducts = response.data;
-            // Сортувати продукти за популярністю
-            const sortedProducts = allProducts.sort(() => Math.random() - 0.5);
-            setProducts(sortedProducts);
-          } catch (error) {
-            console.error("Помилка при отриманні товарів:", error);
-          }
+            try {
+                const response = await lendingData.getProduct();
+                const allProducts = response.data;
+                const sortedProducts = allProducts.sort(() => Math.random() - 0.5);
+                setProducts(sortedProducts);
+            } catch (error) {
+                console.error("Помилка при отриманні товарів:", error);
+            }
         };
-    
         fetchProducts();
-      }, []);
+    }, []);
 
     const filterProductsByCategory = async (category) => {
-        setSelectedCategory(category); // Оновлення обраної категорії
+        setSelectedCategory(category);
         try {
-            const response = await lendingData.getProduct(); // Отримати всі товари
-            const allProducts = response.data; // Отримати дані
+            const response = await lendingData.getProduct();
+            const allProducts = response.data;
             let filteredProducts;
             if (category === "Усі товари") {
-                // Показати всі товари
                 filteredProducts = allProducts.sort(() => Math.random() - 0.5);
             } else {
-                // Показати товари, що належать до обраної категорії
                 filteredProducts = allProducts.filter(product => product.category === category).sort(() => Math.random() - 0.5);
             }
-            // Сортувати товари після фільтрації
             handleSortChange("popularity");
-            setProducts(filteredProducts); // Встановити відфільтровані товари
+            setProducts(filteredProducts);
         } catch (error) {
             console.error("Помилка при отриманні товарів:", error);
         }
     };
-    
+
     const SORT_OPTIONS = {
         popularity: () => products.sort(() => Math.random() - 0.5),
         priceAsc: () => products.sort((a, b) => a.priceProduct - b.priceProduct),
         priceDesc: () => products.sort((a, b) => b.priceProduct - a.priceProduct),
-      };
-      
-      const handleSortChange = (value) => {
-        setSelectedSortOption(value);
-        setProducts(SORT_OPTIONS[value]());
-      };
+    };
 
-const addToCart = (product) => {
-    setCart([...cart, product]);
-    setCartToLocalStorage(product);
-    setAdded(true);
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+        setCartToLocalStorage(product);
+        setTimeout(() => {
+            setCart(prevCart => prevCart.filter(item => item !== product));
+        }, 10000);
+    };
 
-  };
     return (
         <div className="market">
             <div className='content-padding'>
                 <HeaderWhite/>
                 <div className="market-content">
                     <div className="side-bar">                        
-                    <div className="sidebar">
+                        <div className="sidebar">
                             <label>Фільтрація:
-                            <select value={selectedCategory} onChange={(e) => filterProductsByCategory(e.target.value)}>
-                                <option value="Усі товари">Усі товари</option>
-                                <option value="Шипуча суміш для ванни">Шипуча суміш для ванни</option>
-                                <option value="Морська сіль для ванни">Морська сіль для ванни</option>
-                                <option value="Праліне">Праліне</option>
-                                <option value="Бомбочки для ванни">Бомбочки для ванни</option>
-                                <option value="Подарункові бокси">Подарункові бокси</option>
-                                <option value="Розпродаж">Розпродаж</option>
-                            </select>
+                                <select value={selectedCategory} onChange={(e) => filterProductsByCategory(e.target.value)}>
+                                    <option value="Усі товари">Усі товари</option>
+                                    <option value="Шипуча суміш для ванни">Шипуча суміш для ванни</option>
+                                    <option value="Морська сіль для ванни">Морська сіль для ванни</option>
+                                    <option value="Праліне">Праліне</option>
+                                    <option value="Бомбочки для ванни">Бомбочки для ванни</option>
+                                    <option value="Подарункові бокси">Подарункові бокси</option>
+                                    <option value="Розпродаж">Розпродаж</option>
+                                </select>
                             </label>
+
                             <label >Сортування:
-                            <select value={selectedSortOption} onChange={(e) => handleSortChange(e.target.value)}>
-                                <option value="popularity">Популярність</option>
-                                <option value="priceAsc">За зростанням ціни</option>
-                                <option value="priceDesc">За спаданням ціни</option>
-                            </select>
+                                <select value={selectedSortOption} onChange={(e) => handleSortChange(e.target.value)}>
+                                    <option value="popularity">Популярність</option>
+                                    <option value="priceAsc">За зростанням ціни</option>
+                                    <option value="priceDesc">За спаданням ціни</option>
+                                </select>
                             </label>
                         </div>
                     </div>
                     <div className="market-product">
-                        {/* Вікно відтворення товарів */}
                         <div className="product-display">
-                            {/* Відображення списку товарів */}
                             <ul className='market-shop-ul'>
                                 {products.map(product => (
                                     <li key={product._id} className='market-shop-li'>
-                                       <Link to={`/product/${product._id}`} products={product} className='product-link'>
-                                        <img className='market-shop-li-img' src={product.avatarUrl} alt={product.titleProduct} />
-                                        <h3 title={product.titleProduct} className='market-shop-li-h3'>{product.titleProduct}</h3>
-                                        <p className='market-shop-li-category'>{product.category}</p>
-                                        <p className='market-shop-li-price'>{product.priceProduct} грн</p>
+                                        <Link to={`/product/${product._id}`} products={product} className='product-link'>
+                                            <img className='market-shop-li-img' src={product.avatarUrl} alt={product.titleProduct} />
+                                            <h3 title={product.titleProduct} className='market-shop-li-h3'>{product.titleProduct}</h3>
+                                            <p className='market-shop-li-category'>{product.category}</p>
+                                            <p className='market-shop-li-price'>{product.priceProduct} грн</p>
                                         </Link>
-                                       <a className={`market-shop-li-button ${added ? 'added' : ''}`}onClick={() => addToCart(product)}>{added ? 'Додано' : 'Придбати'}</a>
+                                        <button onClick={() => addToCart(product)} className={`market-shop-li-button ${cart.includes(product) ? 'added' : ''}`}>
+                                            {cart.includes(product) ? 'Додано' : 'Придбати'}
+                                        </button>
                                     </li>
                                 ))}
                             </ul>
@@ -119,4 +114,6 @@ const addToCart = (product) => {
         </div>
     );
 };
+
 export default Market;
+
